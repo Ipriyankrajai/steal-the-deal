afb1f1adfaf36926a5727be396e9f823582e915e
Priyank Rajai <priyank73@hotmail.com>
2024-07-16 23:13:54 +0530

    Fix: next auth type error

--- apps/admin/src/auth.ts
+++ apps/admin/src/auth.ts
@@ -1,12 +1,6 @@
-
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-export const { handlers, signIn, signOut, auth }: {
-  handlers: any;
-  signIn: any;
-  signOut: any;
-  auth: any;
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
 })
